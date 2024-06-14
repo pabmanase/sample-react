2c6b063959e5d18859fea204cf33720bf11f1f4e
Pablo <pabmanase@gmail.com>
2024-06-14 13:56:52 -0700

    first commit

---
+++ src/layouts/HelpLayout.js
@@ -0,0 +1,18 @@
+import {NavLink, Outlet } from 'react-router-dom'
+
+export default function HelpLayout() {
+  return (
+    <div className="help-layout">
+
+        <h2>Website Help</h2>
+        <p>We out here helping!</p>
+    
+        <nav>
+            <NavLink to="faq">View the FAQ</NavLink>
+            <NavLink to="contact">Contact Us</NavLink>
+        </nav>
+
+        <Outlet />
+    </div>
+  )
+}
---
+++ src/pages/careers/CareerDetails.js
@@ -0,0 +1,29 @@
+import { useLoaderData } from "react-router-dom"
+
+export default function CareerDetails() {
+    const career = useLoaderData()
+    
+    return (
+        <div className="career-details">
+            <h2>Career Details for {career.title}</h2>
+            <p>Starting salary: {career.salary}</p>
+            <p>Location: {career.location}</p>
+            <div className="details">
+                <p>Details about the job...</p>
+            </div>
+        </div>
+  )
+}
+
+//loader function
+export const careerDetailsLoader = async ({ params }) => {
+    const { id } = params
+
+    const res = await fetch('http://localhost:4000/careers/' + id)
+
+    if (!res.ok) {
+        throw Error('Could not find that career')
+    }
+
+    return res.json()
+}
